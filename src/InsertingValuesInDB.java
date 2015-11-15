fbed4929ec039caaa9c107166a2fcf503166cf78
junaid <junaid.jp@gmail.com>
2015-11-15 13:06:57 -0600

    Inserting User

--- src/InsertingValuesInDB.java
+++ src/InsertingValuesInDB.java
@@ -4,6 +4,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import com.training.hibernate.HibernateSessionFactory;
+import com.training.hibernate.UserBean;
+
 
 public class InsertingValuesInDB {
 	
@@ -11,15 +17,42 @@ public class InsertingValuesInDB {
 		 
 		 // ===== In Java 5, Java.util,Scanner is used for this purpose. 
 	        Scanner in = new Scanner(System.in);
-	        System.out.print("Do you want to Insert a Employee  in DB ?  ");
+	        System.out.print("Do you want to Insert a new User   in DB ?  ");
 	       String check = in.nextLine();
-	       
+	       UserBean userBean = null;
 	       if(check.equalsIgnoreCase("YES")) { 
-	    	   
+	    	   System.out.println("Enter the employee Id");
 	    	    String employeeId  = in.nextLine();
+	    	    
+	    		System.out.println("                                                       ");
+	    	    userBean = new UserBean();
+	    	    userBean.setId(Integer.parseInt(employeeId));
+	    	    
 	    	    System.out.println(employeeId);
+	    	    
+	    	    System.out.println("Enter  the employee Name ");
+	    	    String username = in.nextLine();
+	    		System.out.println("                                                       ");
+	    	    System.out.println(username);
+	    	    
+	    	    userBean.setUsername(username);
+	    	    
+	    	    System.out.println("Enter  the employee password");
+	    	   String password = in.nextLine();
+	    		System.out.println("                                                       ");
+	    	    System.out.println(password);
+	    	    
+	    	    userBean.setPassword(password);
+	    	    
 	       }
-	        System.out.println("You entered : " + check);
+	       
+	       SessionFactory sessionFactory = HibernateSessionFactory.getSessionFactory();
+	       Session session = sessionFactory.openSession();
+	       session.beginTransaction();
+	       session.save(userBean);
+	       session.getTransaction().commit();
+	       session.close();
+	      System.out.println("Value Entered in DB");
 	 
 		 
 	    }
